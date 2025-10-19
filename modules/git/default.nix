f519dea3ae0a6dddeece3779598373b2ea4ffbb6
Masahiro Mori <masahirom0923@gmail.com>
2025-10-19 12:52:55 +0900

    git: add aliases for diff

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -5,6 +5,8 @@
     userName = lib.mkDefault "Your Name";
     userEmail = lib.mkDefault "user@example.com";
     aliases = {
+      d = "diff";
+      dc = "diff --cached";
       graph = "log --graph --all --decorate";
       wt = "worktree";
     };
