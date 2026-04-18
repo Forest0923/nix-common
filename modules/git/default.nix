03d645eaeea26cebd9bdc8963acd55289727695f
Masahiro Mori <47715365+Forest0923@users.noreply.github.com>
2026-04-18 23:20:06 +0900

    feat(git): change commit identities for each git remote host (#21)
    * feat(git): change commit identities for each git remote host
    
    * chore: nix flake update

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -1,24 +1,87 @@
 { config, lib, ... }:
+let
+  inherit (lib) mkAfter mkOption types;
+
+  home_dir = config.home.homeDirectory;
+  ghq_root = "${home_dir}/src";
+
+  host_identities = config.nixCommon.git.hostIdentities;
+  hosts = builtins.attrNames host_identities;
+
+  mk_identity_file = host: {
+    name = "git/identities/${host}.gitconfig";
+    value = {
+      text = ''
+        [user]
+          name = ${builtins.toJSON host_identities.${host}.name}
+          email = ${builtins.toJSON host_identities.${host}.email}
+      '';
+    };
+  };
+
+  mk_include = host: {
+    condition = "gitdir:${ghq_root}/${host}/";
+    path = "${config.xdg.configHome}/git/identities/${host}.gitconfig";
+  };
+in
 {
-  programs.git = {
-    enable = true;
-    settings = {
-      user = {
-        name = lib.mkDefault "";
-        email = lib.mkDefault "";
+  options.nixCommon.git.hostIdentities = mkOption {
+    type = types.attrsOf (types.submodule {
+      options = {
+        name = mkOption {
+          type = types.str;
+          description = "Git user.name for repositories under a specific host path.";
+        };
+        email = mkOption {
+          type = types.str;
+          description = "Git user.email for repositories under a specific host path.";
+        };
       };
-      alias = {
-        d = "diff";
-        dc = "diff --cached";
-        graph = "log --graph --all --decorate";
-        wt = "worktree";
+    });
+    default = { };
+    example = {
+      "github.com" = {
+        name = "Your Name";
+        email = "your.name@example.com";
       };
-      init = {
-        defaultbranch = "main";
-      };
-      ghq = {
-        root = "${config.home.homeDirectory}/src";
+      "gitlab.com" = {
+        name = "Your Name";
+        email = "your.name+gitlab@example.com";
       };
     };
+    description = ''
+      Host-based git identities keyed by host directory under ghq root.
+      For host `github.com`, this module generates:
+      - includeIf condition: `gitdir:${ghq_root}/github.com/`
+      - identity file: `${config.xdg.configHome}/git/identities/github.com.gitconfig`
+    '';
+  };
+
+  config = {
+    programs.git = {
+      enable = true;
+      includes = mkAfter (map mk_include hosts);
+
+      settings = {
+        user = {
+          name = lib.mkDefault "";
+          email = lib.mkDefault "";
+        };
+        alias = {
+          d = "diff";
+          dc = "diff --cached";
+          graph = "log --graph --all --decorate";
+          wt = "worktree";
+        };
+        init = {
+          defaultbranch = "main";
+        };
+        ghq = {
+          root = ghq_root;
+        };
+      };
+    };
+
+    xdg.configFile = builtins.listToAttrs (map mk_identity_file hosts);
   };
 }
